3374ef6ed08aacd65867108a3ee09d9e5b5e2081
cruise12343 <75146325+cruise12343@users.noreply.github.com>
2020-11-28 13:17:36 +0530

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-API_KEY = ''
-API_SECRET = ''
+API_KEY = '6nJSkU9ACfcAYRODLN0V5OuRj3w8kNlCyIZd0uuNw9aKoJSu5muagelw32ay6NaH'
+API_SECRET = 'jJ21PkP3YkbYIxGBnjaTp3dNjpoq0OrGTJmRaq0C7kDYrJbKvBuu6Rz4c0D3xhnb'
